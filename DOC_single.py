1674bdf266ede9974f50ceb65cf3762eafba5b2c
yajumo93 <yajumo93@gmail.com>
2021-07-02 03:40:38 +0000

    .

--- DOC_single.py
+++ DOC_single.py
@@ -3,11 +3,11 @@ import subprocess as sp
 import os
 
 
-input_dir = r'/data_244/WGS/HN00146173/recal_bam/'
-ref_genome = r'/data_244/refGenome/b37/human_g1k_v37.fasta'
+input_dir = r'/home/jun9485/data/WGS/HN00146173/recal_bam/'
+ref_genome = r'/home/jun9485/data/refGenome/b37/human_g1k_v37.fasta'
 output_path = input_dir + 'sample.doc1'
 input_bam_list_file = input_dir + 'bam_list.txt'
-interval_file = r'/data_244/refGenome/b37/SureSelect_v6_processed.bed'
+interval_file = r'/home/jun9485/data/refGenome/b37/SureSelect_v6_processed.bed'
 
 
 seq_type = 'WGS'
