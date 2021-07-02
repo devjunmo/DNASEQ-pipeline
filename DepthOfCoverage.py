7bbbe727237056e3ae3532e7526bf5ae9fc090ed
yajumo93 <yajumo93@gmail.com>
2021-07-02 04:42:25 +0000

    .

--- DepthOfCoverage.py
+++ DepthOfCoverage.py
@@ -2,22 +2,27 @@
 
 import subprocess as sp
 import os
+from glob import glob
 
 
-input_dir = r'/data_244/WGS/HN00146173/recal_bam/'
-ref_genome = r'/data_244/refGenome/b37/human_g1k_v37.fasta'
-output_path = input_dir + 'sample.doc1'
-input_bam_list_file = input_dir + 'bam_list.txt'
-interval_file = r'/data_244/refGenome/b37/SureSelect_v6_processed.bed'
+# input_dir = r'/data_244/WGS/HN00146173/recal_bam/'
+# ref_genome = r'/data_244/refGenome/b37/human_g1k_v37.fasta'
+# output_path = input_dir + 'sample.doc1'
+# input_bam_list_file = input_dir + 'bam_list.txt'
+# interval_file = r'/data_244/refGenome/b37/SureSelect_v6_processed.bed'
+
+input_dir = r'/home/jun9485/data/WGS/HN00146173/recal_bam/'
+input_format = r'recal_*.bam'
+ref_genome = r'/home/jun9485/data/refGenome/b37/human_g1k_v37.fasta'
+output_dir_name = 'DOC_results'
+# output_path = input_dir + 'sample.doc1'
+# input_bam_list_file = input_dir + 'bam_list.txt'
+interval_file = r'/home/jun9485/data/refGenome/b37/SureSelect_v6_processed.bed'
 
 
-output_dir_name = r'maf/'
-tmp_dir = input_dir + 'vcf/'
-fasta_path = r'/data_240/refGenome/b37/human_g1k_v37.fasta'
+qsub_type = 'pbs_old' # 'pbs_pro' (최신, docker) / 'pbs_old' (240서버) 
 
-qsub_type = 'pbs_pro' # 'pbs_pro' (최신, docker) / 'pbs_old' (240서버) 
-
-seq_type = 'WGS'
+seq_type = 'WES'
 
 ####### pbs_pro config ###########
 pbs_N = "mk_maf.WES"
@@ -27,10 +32,11 @@ pbs_l_core = 4
 ##################################
 
 ####### pbs_old config ###########
-qsub_config_file = r''
+qsub_config_file = r'/home/jun9485/src/qsub.5'
 ##################################
 
 
+input_path_lst = glob(input_dir + input_format)
 
 output_dir = input_dir + output_dir_name
 
@@ -38,30 +44,30 @@ if os.path.isdir(output_dir) is False:
     os.mkdir(output_dir)
 
 
-for i in range(len(input_lst)):
+for i in range(len(input_path_lst)):
 
-    f_name = input_lst[i].split(r'/')[-1].split(r'.')[0].split(r'_')[-1] # teratoma-4
-    f_type = input_lst[i].split(r'/')[-1].split(r'.')[0].split(r'_')[-2] # snp/indel
+    f_name = input_path_lst[i].split(r'/')[-1].split(r'.')[0].split(r'_')[-1] # teratoma-4
+    # f_type = input_lst[i].split(r'/')[-1].split(r'.')[0].split(r'_')[-2] # snp/indel
 
-    input_vcf_path = input_lst[i]
-    output_maf_path = output_dir + f_type + '_' + f_name + '.maf'
+    input_bam_path = input_path_lst[i]
+    output_path = output_dir + seq_type + '_' + f_name + '.doc'
 
     # sp.call(rf"perl vcf2maf.pl --input-vcf {input_vcf_path} --output-maf {output_maf_path} --ref-fasta {fasta_path} --tmp-dir {tmp_dir}", shell=True)
 
     if qsub_type == 'pbs_pro':
         
         if seq_type == 'WGS':
-            sp.call(f'echo "gatk DepthOfCoverage -R {ref_genome} -O {output_path} -I {input_bam_list_file}" \
+            sp.call(f'echo "gatk DepthOfCoverage -R {ref_genome} -O {output_path} -I {input_bam_path}" \
                     | qsub -N {pbs_N} -o {pbs_o} -j {pbs_j} -l select={pbs_l_core} &', shell=True)
         
         elif seq_type == 'WES':
-            sp.call(f'echo "gatk DepthOfCoverage -R {ref_genome} -O {output_path} -I {input_bam_list_file} -L {interval_file}" \
+            sp.call(f'echo "gatk DepthOfCoverage -R {ref_genome} -O {output_path} -I {input_bam_path} -L {interval_file}" \
                     | qsub -N {pbs_N} -o {pbs_o} -j {pbs_j} -l select={pbs_l_core} &', shell=True)
     
     elif qsub_type == 'pbs_old':
-        
         if seq_type == 'WGS':
-            sp.call(f'qsub {qsub_config_file} gatk DepthOfCoverage -R {ref_genome} -O {output_path} -I {input_bam_list_file} &', shell=True)
+            sp.call(f'qsub {qsub_config_file} gatk DepthOfCoverage -R {ref_genome} -O {output_path} -I {input_bam_path} &', shell=True)
         
         elif seq_type == 'WES':
-            sp.call(f'qsub {qsub_config_file} python germline_short/variant_calling_single_gs.py -b {bam_file} -n {read_name} -G {OUTPUT_GS_DIR} -R {REF_GENOME_PATH} -L {INTERVAL_FILE_PATH} -y {seq_type} &', shell=True)
+            sp.call(f'qsub {qsub_config_file} gatk DepthOfCoverage -R {ref_genome} -O {output_path} -I {input_bam_path} -L {interval_file} &', shell=True)
+            
