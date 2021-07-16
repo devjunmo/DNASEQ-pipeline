14555a99327f905cce5ff0a635e613e6ba873025
yajumo93 <yajumo93@gmail.com>
2021-07-16 04:00:39 +0000

    .

--- somatic_short/select_variant.py
+++ somatic_short/select_variant.py
@@ -17,11 +17,11 @@ ref_genome_path = ref_dir + 'human_g1k_v37.fasta'
 
 interval_path = ref_dir + r'SureSelect_v6_processed.bed'
 
-# select_type = 'SNP' # INDEL
+select_type = 'SNP' # INDEL
 
 snp_out_dir_name = r'snp/'
-
 indel_out_dir_name = r'indel/'
+
 
 snp_out_dir = input_dir + snp_out_dir_name
 indel_out_dir = input_dir + indel_out_dir_name
@@ -41,7 +41,7 @@ pbs_N = "utuc.mutect2.selectV"
 pbs_o = input_dir + r"pbs_out/"
 pbs_j = "oe"
 pbs_l_core = 2
-SRC_DIR = r"/data_244/src/utuc_pp/DNASEQ-pipeline/somatic_short/"
+SRC_DIR = r"/data_244/src/utuc_select_v/DNASEQ-pipeline/somatic_short/"
 
 if os.path.isdir(pbs_o) is False:
     os.mkdir(pbs_o)
@@ -59,8 +59,29 @@ for i in range(len(input_lst)):
     input_vcf = input_lst[i]
     f_name = input_vcf.split(r'/')[-1].split(r'.')[0]
 
-    # snp
 
-    sp.call
+    # 임시코드로 filtered로 시작되면 어쩌구 코드좀 넣자.. 짜증나서..
+    if f_name.split('_')[0] == 'filtered':
+        f_name = f_name.split('_')[1]
+    
+
+    if select_type == 'SNP':
+
+        output_path_snp = snp_out_dir + 'SNP' + '_' + 'filtered' + '_' + f_name + '.vcf'
+
+        sp.call(rf'echo "sh ../germline_short/select_variants.sh {ref_genome_path} {input_vcf} {output_path_snp} \
+                {select_type} {seq_type} {interval_path}" | qsub \
+                -N {pbs_N} -o {pbs_o} -j {pbs_j} -l ncpus={pbs_l_core} &', shell=True)
+
+
 
     # indel
+
+    if select_type == 'INDEL':
+    
+        output_path_indel = indel_out_dir + 'INDEL' + '_' + 'filtered' + '_' + f_name + '.vcf'
+
+        sp.call(rf'echo "sh ../germline_short/select_variants.sh {ref_genome_path} {input_vcf} {output_path_indel} \
+                {select_type} {seq_type} {interval_path}" | qsub \
+                -N {pbs_N} -o {pbs_o} -j {pbs_j} -l ncpus={pbs_l_core} &', shell=True)
+
