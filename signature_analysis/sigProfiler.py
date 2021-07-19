4449de726a6a509d5ea5ee436a5b8578e5634d63
yajumo93 <yajumo93@gmail.com>
2021-07-19 23:25:23 +0900

    .

--- signature_analysis/sigProfiler.py
+++ signature_analysis/sigProfiler.py
@@ -6,7 +6,18 @@ import os
 
 
 old_wd = os.getcwd()
-os.chdir(r'/home/jun9485/data/utuc/somatic_call/filtered/snp')
+# os.chdir(r'/home/jun9485/data/utuc/somatic_call/filtered/snp')
+
+# root_dir = r'/home/jun9485/data/utuc/somatic_call/filtered/'
+# root_dir = r'/home/jun9485/'
+root_dir = r'/home/jun9485/data/utuc/somatic_call_mutect1/filtered_vcf/'
+
+# input_dir_name = r'snp/'
+# input_dir_name = r'ejTest/'
+input_dir_name = r'vcf/'
+
+os.chdir(root_dir)
+
 os.getcwd()
 
 
@@ -14,15 +25,15 @@ os.getcwd()
 
 # path_to_example_table = sig.importdata("vcf")
 
-path_to_example_table = sig.importdata("vcf")
-print(path_to_example_table)
+# path_to_example_table = sig.importdata("vcf")
+# print(path_to_example_table)
 
 out_dir_name = r'sig_analysis/'
 
 # input_dir_name = r''
 
+input_dir = root_dir + input_dir_name
 
-
-sig.sigProfilerExtractor('vcf', out_dir_name, path_to_example_table)
+sig.sigProfilerExtractor('vcf', out_dir_name, input_dir)
 
 os.chdir(old_wd)
