126abb21171b01cec3ec2e9c02623acf7de4995e
yajumo93 <yajumo93@gmail.com>
2022-04-30 15:36:07 +0900

    .

--- somatic_short/Varscan/get_mutation_count.py
+++ somatic_short/Varscan/get_mutation_count.py
@@ -2,8 +2,8 @@ import os
 import pandas as pd
 from glob import glob
 
-root_dir = r'E:/stemcell/somatic_analysis/varscan'
-file_format = r'*.snp'
+root_dir = r'E:/stemcell/somatic_analysis/varscan/snp'
+file_format = r'*.vcf'
 
 file_lst = glob(os.path.join(root_dir, file_format))
 
@@ -12,9 +12,10 @@ res_df = pd.DataFrame(columns=['Germline', 'LOH', 'Somatic', 'Unknown'])
 for f in file_lst:
 
     vcf_df = pd.read_csv(f, sep='\t')
-    print(f)
     sample_name = f.split('\\')[-1].split(r'.')[0] # 유동적
     print(sample_name)
+    
+    vcf_df = vcf_df[vcf_df['normal_reads2'] <= 1]
     
     germline_count = vcf_df[vcf_df['somatic_status']=='Germline'].shape[0]
     loh_count = vcf_df[vcf_df['somatic_status']=='LOH'].shape[0]
@@ -27,6 +28,6 @@ for f in file_lst:
 
 print(res_df)
 
-output_dir = os.path.join(root_dir, 'varscan_muation_count.csv')
+output_path = os.path.join(root_dir, 'varscan_muation_count_nv1.csv')
 
-res_df.to_csv(output_dir)
+res_df.to_csv(output_path)
